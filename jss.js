f8160749fc5af01dd1d6eb3dd16c609af95e0e08
lickinfingerss <79010286+lickinfingerss@users.noreply.github.com>
2021-03-01 10:17:49 +0100

    first upload

---
+++ jss.js
@@ -0,0 +1,23 @@
+
+    
+        //main top naviigationn
+        
+        function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+   //slid in overly
+    function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+   
